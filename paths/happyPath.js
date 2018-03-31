f760939b998b177aa8b120b9867ba7e087ce37c0
Roopa Devi <roopa@Roopas-MacBook-Pro.local>
2018-03-31 19:37:05 +0100

    added negative scenario

--- paths/happyPath.js
+++ paths/happyPath.js
@@ -1,5 +1,5 @@
 
-Feature('Judicial UI E2E Tests');
+Feature('login functionality');
 
 
 Scenario("Test Homepage & Signin", function*(I) {
@@ -14,6 +14,31 @@ Scenario("Test Homepage & Signin", function*(I) {
     I.click('Sign in');
     I.wait(30);
     I.seeElement('h1');
+
+
+});
+
+
+Scenario("Invalid Signin", function*(I) {
+
+    I.amOnPage("/");
+    browser.ignoreSynchronization = true;
+    browser.waitForAngular();
+    browser.sleep(500);
+    I.wait(30);
+    I.fillField("#username", "testccd@gmail.com");
+    I.fillField("#password", 'Monda567');
+    I.click('Sign in');
+    I.wait(30);
+    I.seeElement('.error-summary');
+    I.see('\n' +
+        '                        Incorrect email/password combination\n' +
+        '                    ', 'h3#failure-error-summary-heading');
+    I.see('\n' +
+        '                        Please check your email address and password and try again:\n' +
+        '                    ', '.error-summary > p');
+
+    I.dontSeeElement('h1');
 
 
 });
