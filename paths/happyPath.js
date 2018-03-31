dfa603f898b0d5edf64affe4ba33e33710f488aa
Roopa Devi <roopa@roopas-mbp.home>
2018-03-31 22:53:59 +0100

    adding failed png

--- paths/happyPath.js
+++ paths/happyPath.js
@@ -1,44 +1,62 @@
+Feature('CCD login logout & feedback functionality');
 
-Feature('login functionality');
 
-
-Scenario("Test Homepage & Signin", function*(I) {
+Scenario("ccd Signin & Signout", function* (I) {
 
     I.amOnPage("/");
     browser.ignoreSynchronization = true;
     browser.waitForAngular();
     browser.sleep(500);
-    I.wait(30);
+    I.wait(10);
     I.fillField("#username", "nybgul@gmail.com");
     I.fillField("#password", 'Monday01');
     I.click('Sign in');
-    I.wait(30);
-    I.seeElement('h1');
+    I.wait(10);
+    I.see('Case List', 'h1');
+    I.click('button.dropbtn');
+    I.click('Sign Out');
+    I.see('Your session has been closed', 'h1.heading-large');
+    I.see('You have succesfully logged out and this window can now be closed.', 'p.lede');
+
+});
+
+
+Scenario("Invalid Signin", function* (I) {
+
+    I.amOnPage("/");
+    browser.ignoreSynchronization = true;
+    browser.waitForAngular();
+    browser.sleep(500);
+    I.wait(10);
+    I.fillField("#username", "testccd@gmail.com");
+    I.fillField("#password", 'Monda567');
+    I.click('Sign in');
+    I.seeElement('.error-summary');
+    I.see('Incorrect email/password combination', 'h3#failure-error-summary-heading');
+    I.see('Please check your email address and password and try again:', '.error-summary > p');
+    I.dontSee('Sign Out');
 
 
 });
 
 
-Scenario("Invalid Signin", function*(I) {
+Scenario("feedback", function* (I) {
 
     I.amOnPage("/");
     browser.ignoreSynchronization = true;
     browser.waitForAngular();
     browser.sleep(500);
-    I.wait(30);
-    I.fillField("#username", "testccd@gmail.com");
-    I.fillField("#password", 'Monda567');
-    I.click('Sign in');
-    I.wait(30);
-    I.seeElement('.error-summary');
-    I.see('\n' +
-        '                        Incorrect email/password combination\n' +
-        '                    ', 'h3#failure-error-summary-heading');
-    I.see('\n' +
-        '                        Please check your email address and password and try again:\n' +
-        '                    ', '.error-summary > p');
-
-    I.dontSeeElement('h1');
+    I.wait(10);
+    I.click('feedback');
+    I.wait(10);
+    I.amOnPage("http://www.smartsurvey.co.uk/s/58DYD/");
+    I.seeElement('div.tLogo > img');
+    I.see('Help us improve this service', 'h1.ss-survey-title');
+    I.click('input#o540869344974458');
+    I.fillField('#t44943949', 'test');
+    I.click('input#cmdGo');
+    I.wait(10);
+    I.see('Thank you for your feedback', 'h2');
 
 
 });
